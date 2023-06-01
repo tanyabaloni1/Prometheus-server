a41dea3b4e6eaf0c8d075b80ce84c6e3ce24ed47
ShradhaGupta26 <88206733+ShradhaGupta26@users.noreply.github.com>
2023-06-01 12:06:10 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "template_file" "promscript" {
 template = file("${path.module}/user_data.sh")
 vars = {
- elasticsearch_private_ip= module.elasticsearch.ec2_elasticsearch_private_ip
+ elasticsearch_private_ip= var.elasticsearch_private_ip
 }
 }
